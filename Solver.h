4689dadda03ea92aa2bb3fa13f26a25e1c38b80d
gidzzz <grg.gdl@gmail.com>
2013-11-17 15:03:15 +0100

    Separated the sequential solver from main.cpp and added a stub for the parallel solver.

---
+++ Solver.h
@@ -0,0 +1,63 @@
+#ifndef SOLVER_H
+#define SOLVER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "State.h"
+
+using namespace std;
+
+class Solver {
+
+protected:
+
+    const string FILE_NAME;
+
+    int size;
+    vector<bool> fine_points;
+    vector<State> stack;
+
+public:
+
+    Solver(const string &file_name) : FILE_NAME(file_name) {
+        ifstream f(file_name.c_str());
+
+        string content;
+        f >> content;
+        f >> size;
+        f >> content;
+
+        fine_points = vector<bool>(size * size, true);
+        while (f.good()) {
+            int x, y;
+            f >> x >> y;
+            fine_points[((x - 1) * size + (y - 1))] = false;
+        }
+    }
+
+    virtual bool solve() {
+        stack.push_back(State(size, -1));
+
+        while (stack.size() > 0) {
+            State current = stack.back();
+            stack.pop_back();
+            if (current.is_valid()) {
+                if (!current.is_last()) {
+                    current.unpackToStack(stack, fine_points);
+                } else {
+                    current.print();
+                    return true;
+                }
+            }
+        }
+
+        cout << "There is no possible solution for " << size << "-queen problem defined in: "
+             << FILE_NAME << endl;
+
+        return false;
+    }
+};
+
+#endif // SOLVER_H
--- main.cpp
+++ main.cpp
@@ -1,58 +1,23 @@
 #include <string>
 #include <vector>
-#include <fstream>
-#include <iostream>
 
-#include "State.h"
+#include <mpi.h>
+
+#include "ParallelSolver.h"
 
 using namespace std;
 
-vector<bool> read_file(string &file_path, int &size);
-
 int main(int argc, char** argv) {
-    string FILE_NAME = "queen4";
-    vector<State> stack;
-    int size;
+    MPI_Init(&argc, &argv);
 
-    vector<bool> fine_points = read_file(FILE_NAME, size);
+    int comm_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 
-    stack.push_back(State(size, -1));
-//    cout << "debug: stack initialized" << endl;
-    while (stack.size() > 0) {
-        State current = stack.back();
-        stack.pop_back();
-        if (current.is_valid()) {
-            if (!current.is_last()) {
-                current.unpackToStack(stack, fine_points);
-            } else {
-                current.print();
-                return 0;
-            }
-        }
-    }
+    const string FILE_NAME = "queen4";
 
-    cout << "There is no possible solution for " << size << "-queen problem defined in: "
-         << FILE_NAME << endl;
+    comm_size > 1 ? ParallelSolver(FILE_NAME).solve()
+                  : Solver(FILE_NAME).solve();
+
+    MPI_Finalize();
     return 0;
-}
-
-vector<bool> read_file(string &file_name, int &size) {
-    string content;
-    int x;
-    int y;
-
-    ifstream f(file_name.c_str());
-    f >> content;
-    f >> size;
-    f >> content;
-
-    vector<bool> fine_points(size * size, true);
-    while (!f.eof()) {
-        f >> x;
-        f >> y;
-        fine_points[((x - 1) * size + (y - 1))] = false;
-//        cout << "debug: beaned x=" << x << ", y=" << y << endl;
-    }
-//    cout << "debug: file red" << endl;
-    return fine_points;
 }

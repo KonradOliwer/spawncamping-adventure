ccc93921591b5551caafec1911ddb4cef5a4af22
gidzzz <grg.gdl@gmail.com>
2013-11-17 19:41:00 +0100

    Fixed typos and compilation errors in the token module.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 spawncamping-adventure
 ======================
-Na razie dodałem obsługę tokenów. Powinno działać, ale nie mogę teraz przetesować (bez reszty implementacji). Przy deklaracji metod masz napisane kiedy i co użyć.
+Już się przynajmniej kompiluje. ;)
+
+Wymiana zadań jest na razie w gałęzi work-exchange-demo, bo połączenie tego w całość chwilkę może zająć.
--- multi.cpp
+++ multi.cpp
@@ -11,14 +11,13 @@ static const int TOKEN_MSG = 1991;
 //**********************************************************************************************
 // Private! Don't call it:
 void reciveToken(int source, int &token, MPI_Comm comm, MPI_Status status);
-void passToeken(int destiny, int color, int &token, MPI_Comm comm);
+void passToken(int destination, int &color, int &token, MPI_Comm comm);
 
-// use always when process recive work:
+// Use always when process receives work:
 void reciveWorkEvent(int myrank, int source, int &color);
 
-// use always when process turn inactive:
-void serveToken(int &color, int &token, int myrank, int ring_size, MPI_Comm comm, MPI_Status status,
-        bool &fail);
+// Use always when process turns inactive:
+void serveToken(int &color, int &token, int myrank, int ring_size, MPI_Comm comm, MPI_Status status, bool &fail);
 //**********************************************************************************************
 
 void reciveWorkEvent(int myrank, int source, int &color) {
@@ -27,11 +26,11 @@ void reciveWorkEvent(int myrank, int source, int &color) {
     }
 }
 
-void passToeken(int destiny, int color, int &token, MPI_Comm comm) {
+void passToken(int destination, int &color, int &token, MPI_Comm comm) {
     if (color = BLACK) {
         token = BLACK;
     }
-    MPI_Isend(&token, 1, MPI_INT, destiny, TOKEN_MSG, comm); //blocking
+    MPI_Send(&token, 1, MPI_INT, destination, TOKEN_MSG, comm); //blocking
     token = NO_TOKEN;
     color = WHITE;
 }
@@ -40,7 +39,7 @@ void reciveToken(int source, int &token, MPI_Comm comm, MPI_Status status) {
     MPI_Request request;
     int flag = MPI_Probe(source, TOKEN_MSG, comm, &status);
     if (flag) {
-        MPI_Recv(&token, 1, MPI_INT, int source, int TOKEN_MSG, &status); //blocking
+        MPI_Recv(&token, 1, MPI_INT, source, TOKEN_MSG, comm, &status); //blocking
     }
 }
 
@@ -49,9 +48,9 @@ void serveToken(int &color, int &token, int myrank, int ring_size, MPI_Comm comm
         color = WHITE;
     }
     if (token != NO_TOKEN) {
-        passToeken((myrank + 1) % ring_size, &color, &token, comm);
+        passToken((myrank + 1) % ring_size, color, token, comm);
     } else {
-        reciveToken(myrank % ring_size, &token, comm, status);
+        reciveToken(myrank % ring_size, token, comm, status);
         fail = myrank == 0 && token == WHITE; //should imply finalizing program with fail message
     }
 }
